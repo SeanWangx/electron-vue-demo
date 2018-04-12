c4ab80e51b29ca133eb949fdb3da9e6e1e89e2cb
SeanWangx <1971822513@qq.com>
2018-04-12 22:49:50 +0800

    上传文件页面完成

--- src/renderer/views/Buckets/Upload.vue
+++ src/renderer/views/Buckets/Upload.vue
@@ -1,74 +1,136 @@
 <template>
   <el-main style="font-size: 0;height: 100%;position: relative;">
-    <div style="margin-bottom: 10px;position: relative;">
-      <el-button size="small" @click="back" icon="el-icon-back"></el-button>
+    <div class="upload-title">
+      <span>上传文件</span>
+      <i class="el-icon-close upload-close" @click="close"></i>
+      <!-- <el-button type="text" class="upload-close" size="large" @click="close" icon="el-icon-close"></el-button> -->
     </div>
     <div class="upload-container">
       <el-upload
-        class="upload-demo"
+        class="upload-content"
         action="https://jsonplaceholder.typicode.com/posts/"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :before-remove="beforeRemove"
-        multiple
-        :limit="3"
-        :on-exceed="handleExceed"
-        :file-list="fileList">
-        <el-button size="small" type="primary">点击上传</el-button>
-        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        :on-preview="handlePreview">
+        <el-button size="small" type="primary">选择文件</el-button>
+        <div slot="tip" class="el-upload__tip">网页不能上传超过 500M 的文件，大文件请<span @click="toTip" class="upload-tip"> 使用工具 </span>上传</div>
       </el-upload>
+      <div class="upload-config">
+        <div class="upload-ctype">
+          <div>选择上传文件存储类型:</div>
+          <div>
+            <el-radio-group v-model="ctype" size="small">
+              <el-radio label="0" border>标准存储</el-radio>
+              <el-radio label="1" border>低频存储</el-radio>
+            </el-radio-group>
+          </div>
+        </div>
+        <el-button size="small" @click="refresh = !refresh">{{ refresh }}</el-button> 
+      </div>
     </div>
-    <!-- <el-button size="small" @click="refresh = !refresh">{{ refresh }}</el-button> -->
   </el-main>
 </template>
 
 <script>
+import { shell } from 'electron'
 export default {
   data () {
     return {
       refresh: false,
-      fileList: [
-        {
-          name: 'food.jpeg',
-          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-        },
-        {
-          name: 'food2.jpeg',
-          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-        }
-      ]
+      ctype: '0'
     }
   },
   deactivated () {
     this.refresh = false
   },
   methods: {
-    back () {
+    close () {
       this.$emit('change', { view: 'VContent', refresh: this.refresh })
     },
-    handleRemove (file, fileList) {
-      console.log(file, fileList)
+    toTip () {
+      shell.openExternal('https://developer.qiniu.com/sdk#official-tool')
     },
     handlePreview (file) {
       console.log(file)
-    },
-    handleExceed (files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
-    },
-    beforeRemove (file, fileList) {
-      return this.$confirm(`确定移除 ${file.name}？`)
     }
   }
 }
 </script>
 
 <style scoped>
+.upload-title {
+    flex: 0 0 100%;
+    width: 100%;
+    height: 22px;
+    margin: 0;
+    padding: 30px 0;
+    font-weight: 400;
+    line-height: 22px;
+    font-size: 0;
+    text-align: center;
+    position: relative;
+}
 .upload-container {
-    display: block;
+    display: flex;
+    flex-wrap: wrap;
     position: relative;
     width: 100%;
-    height: calc(100% - 48px);
+    height: calc(100% - 82px);
     margin: 0;
     padding: 0;
+}
+.upload-title > span {
+    font-size: 16px;
+}
+.upload-close {
+    position: absolute;
+    right: 0;
+    top: 0;
+    font-size: 52px;
+    color: #d3d3d3;
+    cursor: pointer;
+}
+.upload-close:hover {
+    color: #999;
+}
+.upload-content {
+    flex: 0 0 60%;
+    width: 60%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    position: relative;
+}
+.upload-config {
+    flex: 0 0 40%;
+    width: 40%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 20px;
+    position: relative;
+    box-sizing: border-box;
+}
+.upload-ctype {
+    font-size: 14px;
+}
+.upload-ctype > div {
+    margin: 0 0 20px 0;
+}
+.upload-tip {
+    cursor: pointer;
+    color: #409EFF;
+    font-weight: 800;
+}
+</style>
+
+<style>
+.upload-content .el-upload-list {
+    background: #fff !important;
+    width: 100%;
+    height: calc(100% - 72px);
+    margin: 16px 0 0 0;
+    padding: 0 10px 10px 10px;
+    border: 1px solid #dcdfe5;
+    border-radius: 4px;
+    box-sizing: border-box;
+    overflow-y: auto;
 }
 </style>
