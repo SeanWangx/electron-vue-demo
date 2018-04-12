0cf8f23a09b62386612c83619c48350ca7ff68db
SeanWangx <1971822513@qq.com>
2018-04-12 23:59:23 +0800

    完成基本上传功能

--- src/renderer/views/Buckets/Upload.vue
+++ src/renderer/views/Buckets/Upload.vue
@@ -9,8 +9,13 @@
       <el-upload
         ref="upload"
         class="upload-content"
-        action="https://jsonplaceholder.typicode.com/posts/"
-        :on-preview="handlePreview">
+        name="file"
+        :data="uploadData"
+        :action="uploadURL"
+        :before-upload="beforeUpload"
+        :on-preview="handlePreview"
+        :on-success="handleSuccess"
+        :on-error="handleError">
         <el-button size="small" type="primary">选择文件</el-button>
         <div slot="tip" class="el-upload__tip">网页不能上传超过 500M 的文件，大文件请<span @click="toTip" class="upload-tip"> 使用工具 </span>上传</div>
       </el-upload>
@@ -24,24 +29,56 @@
             </el-radio-group>
           </div>
         </div>
-        <el-button size="small" @click="refresh = !refresh">{{ refresh }}</el-button> 
+        <el-button @click="test">Test</el-button>
       </div>
     </div>
   </el-main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { shell } from 'electron'
+import { getUploadToken } from '@/utils/tools'
+
+const ZONE_UPLOAD = {
+  'z0': 'up.qiniup.com',
+  'z1': 'up-z1.qiniup.com',
+  'z2': 'up-z2.qiniup.com',
+  'na0': 'up-na0.qiniup.com',
+  'as0': 'up-as0.qiniup.com'
+}
+
 export default {
+  props: {
+    bucket: {
+      type: String,
+      required: true
+    },
+    zone: {
+      type: String,
+      required: true
+    }
+  },
   data () {
     return {
       refresh: false,
-      ctype: '0'
+      ctype: '0',
+      uploadData: {}
+    }
+  },
+  computed: {
+    ...mapGetters({
+      accessKey: 'accessKey',
+      secretKey: 'secretKey'
+    }),
+    uploadURL () {
+      return !!this.zone === true ? `http://${ZONE_UPLOAD[this.zone]}` : ''
     }
   },
   deactivated () {
     this.refresh = false
     this.ctype = '0'
+    this.uploadData = {}
     this.$refs['upload'].clearFiles()
   },
   methods: {
@@ -51,8 +88,26 @@ export default {
     toTip () {
       shell.openExternal('https://developer.qiniu.com/sdk#official-tool')
     },
+    beforeUpload (file) {
+      console.log(file)
+      let uploadToken = getUploadToken(this.bucket, file.name, this.accessKey, this.secretKey)
+      this.$set(this.uploadData, 'key', file.name)
+      this.$set(this.uploadData, 'token', uploadToken)
+    },
     handlePreview (file) {
       console.log(file)
+    },
+    handleSuccess (...args) {
+      console.log(args)
+      this.refresh = true
+      this.$message.success('上传文件成功！')
+    },
+    handleError (...args) {
+      console.log(args)
+      this.$message.error('上传文件失败！')
+    },
+    test () {
+      console.log(this.bucket, this.uploadURL)
     }
   }
 }
